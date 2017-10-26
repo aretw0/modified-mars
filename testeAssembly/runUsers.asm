1b4057c7e952d4f1218ebd6182565ef9cbc956aa
Arthur Silva <aretw0@hotmail.com>
2017-10-26 19:14:56 -0300

    Upando runUser final

--- testeAssembly/runUsers.asm
+++ testeAssembly/runUsers.asm
@@ -47,11 +47,17 @@ body1: .asciiz "The quick brown fox jumps over the lazy dog " # tem 44 caractere
 body2: .asciiz "The slowly red fox was taken by the lazy dog" # tem 44 caracteres
 body3: .asciiz "The quick brown dog knocked the lazy red fox" # tem 44 caracteres
 
+bodySp: .asciiz "Eu amei pagar a cadeira de sistemas operacionais com meu querido professor Silvio!"
+# 82
+
+bodySp1: .asciiz "Work it, make it, do it, makes us\nHarder, better, faster, stronger\nMore than, our, hour, ever\nEver, after, work is, over\nWork it, make it, do it, makes us\nHarder, better, faster, stronger\nWork it harder, make it better\nDo it faster, makes us stronger\nMore than ever, hour after hour\nWork is never over\nWork it harder, make it better\nDo it faster, makes us stronger\nMore than ever, hour after hour\nWork is never over" 
+# 428
+
 .text
 #criação dos processos
 	fork(Programa1, Programa2, 8, 0, 15)
-#	fork(Programa2, Programa3, 10, 0, 15)
-#	fork(Programa3, FimPrograma3, 12, 0, 15)
+	fork(Programa2, Programa3, 10, 0, 15)
+	fork(Programa3, FimPrograma3, 12, 0, 15)
 	fork(Idle,Programa1, 6, 0, 15)		
 #escalonando o primeiro processo	
 	processChange
@@ -70,6 +76,38 @@ rightOpen11:
 rightWrite11:
 	prinThis(writeOk)
 	closing(closeOk)
+	opening(file12,9,warnO,rightOpen12,fimAll1)
+rightOpen12:
+	add $s0,$v0,$zero # salvando file description
+	prinThis(openOk)
+	writing(body1,44,warnW,rightWrite12,fimAll1)
+rightWrite12:
+	prinThis(writeOk)
+	closing(closeOk)
+	opening(file13,9,warnO,rightOpen13,fimAll1)
+rightOpen13:
+	add $s0,$v0,$zero # salvando file description
+	prinThis(openOk)
+	writing(body1,44,warnW,rightWrite13,fimAll1)
+rightWrite13:
+	prinThis(writeOk)
+	closing(closeOk)
+	opening(file14,9,warnO,rightOpen14,fimAll1)
+rightOpen14:
+	add $s0,$v0,$zero # salvando file description
+	prinThis(openOk)
+	writing(body1,44,warnW,rightWrite14,fimAll1)
+rightWrite14:
+	prinThis(writeOk)
+	closing(closeOk)
+	opening(file15,9,warnO,rightOpen15,fimAll1)
+rightOpen15:
+	add $s0,$v0,$zero # salvando file description
+	prinThis(openOk)
+	writing(bodySp1,428,warnW,rightWrite15,fimAll1)
+rightWrite15:
+	prinThis(writeOk)
+	closing(closeOk)
 	opening(file11,0,warnO,rightOpenR1,fimAll1)
 rightOpenR1:
 	add $s0,$v0,$zero # salvando file description
@@ -80,8 +118,7 @@ rightRead1:
 	prinThis(endl)
 	prinThis(output1)
 	prinThis(endl)
-
-
+	closing(closeOk)
 fimAll1:
 	closing(closeOk)
 	processTerminate
@@ -95,6 +132,38 @@ rightOpen21:
 rightWrite21:
 	prinThis(writeOk)
 	closing(closeOk)
+	opening(file22,9,warnO,rightOpen22,fimAll2)
+rightOpen22:
+	add $s0,$v0,$zero # salvando file description
+	prinThis(openOk)
+	writing(body2,44,warnW,rightWrite22,fimAll2)
+rightWrite22:
+	prinThis(writeOk)
+	closing(closeOk)
+	opening(file23,9,warnO,rightOpen23,fimAll2)
+rightOpen23:
+	add $s0,$v0,$zero # salvando file description
+	prinThis(openOk)
+	writing(body2,44,warnW,rightWrite23,fimAll2)
+rightWrite23:
+	prinThis(writeOk)
+	closing(closeOk)
+	opening(file24,9,warnO,rightOpen24,fimAll2)
+rightOpen24:
+	add $s0,$v0,$zero # salvando file description
+	prinThis(openOk)
+	writing(body1,44,warnW,rightWrite24,fimAll2)
+rightWrite24:
+	prinThis(writeOk)
+	closing(closeOk)
+	opening(file25,9,warnO,rightOpen25,fimAll2)
+rightOpen25:
+	add $s0,$v0,$zero # salvando file description
+	prinThis(openOk)
+	writing(bodySp,82,warnW,rightWrite25,fimAll2)
+rightWrite25:
+	prinThis(writeOk)
+	closing(closeOk)
 	opening(file21,0,warnO,rightOpenR2,fimAll2)
 rightOpenR2:
 	add $s0,$v0,$zero # salvando file description
@@ -105,9 +174,6 @@ rightRead2:
 	prinThis(endl)
 	prinThis(output2)
 	prinThis(endl)
-
-
-	
 fimAll2:
 	closing(closeOk)
 	processTerminate
@@ -121,6 +187,38 @@ rightOpen31:
 rightWrite31:
 	prinThis(writeOk)
 	closing(closeOk)
+	opening(file32,9,warnO,rightOpen32,fimAll3)
+rightOpen32:
+	add $s0,$v0,$zero # salvando file description
+	prinThis(openOk)
+	writing(body3,44,warnW,rightWrite32,fimAll3)
+rightWrite32:
+	prinThis(writeOk)
+	closing(closeOk)
+	opening(file33,9,warnO,rightOpen33,fimAll3)
+rightOpen33:
+	add $s0,$v0,$zero # salvando file description
+	prinThis(openOk)
+	writing(body3,44,warnW,rightWrite33,fimAll3)
+rightWrite33:
+	prinThis(writeOk)
+	closing(closeOk)
+	opening(file34,9,warnO,rightOpen34,fimAll3)
+rightOpen34:
+	add $s0,$v0,$zero # salvando file description
+	prinThis(openOk)
+	writing(body3,44,warnW,rightWrite34,fimAll3)
+rightWrite34:
+	prinThis(writeOk)
+	closing(closeOk)
+	opening(file35,9,warnO,rightOpen35,fimAll3)
+rightOpen35:
+	add $s0,$v0,$zero # salvando file description
+	prinThis(openOk)
+	writing(body2,44,warnW,rightWrite35,fimAll3)
+rightWrite35:
+	prinThis(writeOk)
+	closing(closeOk)
 	opening(file31,0,warnO,rightOpenR3,fimAll3)
 rightOpenR3:
 	add $s0,$v0,$zero # salvando file description
@@ -131,8 +229,6 @@ rightRead3:
 	prinThis(endl)
 	prinThis(output3)
 	prinThis(endl)
-
-
 fimAll3:
 	closing(closeOk)
 	processTerminate
